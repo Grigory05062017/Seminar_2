adab44c137f5db12ca4854713d2f9d5a4154ae17
Grigory <free_time_88@mail.ru>
2023-07-09 16:15:01 +0500

    task15

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -9,6 +9,10 @@ Console.Clear();
 Console.Write("Введите цифру, обозначающую порядковый номер дня недели (от 1 до 7): ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if (n==6 || n==7)
-    Console.WriteLine("да");
-else Console.WriteLine("нет");
+if (n>=6 && n<=7)
+    {
+        if (n==6 || n==7)
+        Console.WriteLine("да");
+        else Console.WriteLine("нет");
+    }
+else Console.WriteLine("неверное число");
